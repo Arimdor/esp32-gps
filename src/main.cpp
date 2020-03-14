3d918da53538ad3bc71e903cd3fdb0ae1ab8c7ab
Rodrigo De La Flor <rodrigo.120894@gmail.com>
2020-03-14 15:08:06 -0500

    tinyGPS++ implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,132 @@
 #include <Arduino.h>
 #include <ArduinoHttpClient.h>
-
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 #define TINY_GSM_MODEM_SIM800
 #include <TinyGsmClient.h>
 #define SerialAT Serial1
 
 // TTGO T-Call pins
-#define MODEM_RST            5
-#define MODEM_PWKEY          4
-#define MODEM_POWER_ON       23
-#define MODEM_TX             27
-#define MODEM_RX             26
-#define I2C_SDA              21
-#define I2C_SCL              22
+#define MODEM_RST 5
+#define MODEM_PWKEY 4
+#define MODEM_POWER_ON 23
+#define MODEM_TX 27
+#define MODEM_RX 26
+#define I2C_SDA 21
+#define I2C_SCL 22
 
-const char apn[]      = ""; // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
+const char apn[] = "";      // APN (example: internet.vodafone.pt) use https://wiki.apnchanger.org
 const char gprsUser[] = ""; // GPRS User
 const char gprsPass[] = ""; // GPRS Password
-const char simPIN[]   = ""; // Pin de SIM
+const char simPIN[] = "";   // Pin de SIM
 
 const char server[] = "jsonplaceholder.typicode.com"; // domain name: example.com, maker.ifttt.com, etc
-const char resource[] = "/todos/1";         // resource path, for example: /post-data.php
-const int  port = 80;
+const char resource[] = "/todos/1";                   // resource path, for example: /post-data.php
+const int port = 80;
 
 const uint32_t rate = 0;
 TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
 HttpClient http(client, server, port);
 
-void setup() {
-   Serial.begin(115200);
-   Serial.println("setup starting...");
-   delay(3000);
+//GPS VARS
+HardwareSerial SerialGPS(1);
+static const int RXD2 = 16, TXD2 = 17;
 
-   // Set modem reset, enable, power pins
+TinyGPSPlus gps;
+
+void requestApiLocation()
+{
+  Serial.print("Connecting to APN");
+  Serial.print(apn);
+  if (!modem.gprsConnect(apn, gprsUser, gprsPass))
+  {
+    Serial.println("Fail");
+  }
+  else
+  {
+    // Making an HTTP POST request
+    Serial.println("GPRS connected");
+
+    Serial.print(F("Starting web client"));
+    http.connectionKeepAlive(); // Currently, this is needed for HTTPS
+    int err = http.get(resource);
+    if (err != 0)
+    {
+      Serial.println(F("Failed to connect"));
+      delay(10000);
+      return;
+    }
+
+    int status = http.responseStatusCode();
+    Serial.print(F("Response status code: "));
+    Serial.println(status);
+    if (!status)
+    {
+      delay(10000);
+      return;
+    }
+
+    Serial.println(F("Response Headers:"));
+    while (http.headerAvailable())
+    {
+      String headerName = http.readHeaderName();
+      String headerValue = http.readHeaderValue();
+      Serial.println("    " + headerName + " : " + headerValue);
+    }
+
+    int length = http.contentLength();
+    if (length >= 0)
+    {
+      Serial.print(F("Content length is: "));
+      Serial.println(length);
+    }
+    if (http.isResponseChunked())
+    {
+      Serial.println(F("The response is chunked"));
+    }
+
+    String body = http.responseBody();
+    Serial.println(F("Response:"));
+    Serial.println(body);
+
+    Serial.print(F("Body length is: "));
+    Serial.println(body.length());
+
+    // Shutdown
+    http.stop();
+    Serial.println(F("Web client shutdown"));
+
+    modem.gprsDisconnect();
+    Serial.println(F("GPRS disconnected"));
+  }
+}
+
+
+void getGPSData()
+{
+  while (SerialGPS.available() > 0)
+  {
+    gps.encode(SerialGPS.read());
+  }
+
+  Serial.print("LAT=");
+  Serial.println(gps.location.lat(), 6);
+  Serial.print("LONG=");
+  Serial.println(gps.location.lng(), 6);
+  Serial.print("ALT=");
+  Serial.println(gps.altitude.meters());
+}
+
+/* Metodos de Arduino */
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Setup starting...");
+  SerialGPS.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  delay(5000);
+
+  /* // Set modem reset, enable, power pins
   pinMode(MODEM_PWKEY, OUTPUT);
   pinMode(MODEM_RST, OUTPUT);
   pinMode(MODEM_POWER_ON, OUTPUT);
@@ -52,83 +145,34 @@ void setup() {
   // use modem.init() if you don't need the complete restart
 
   String modemInfo = modem.getModemInfo();
-  Serial.print("Modem Info: ");
+  Serial.print("Modem Info");
   Serial.println(modemInfo);
 
   // Unlock your SIM card with a PIN if needed
-  if (strlen(simPIN) && modem.getSimStatus() != 3 ) {
+  if (strlen(simPIN) && modem.getSimStatus() != 3)
+  {
     modem.simUnlock(simPIN);
   }
 
   Serial.print("Waiting for network...");
-  if (!modem.waitForNetwork()) {
-    Serial.println(" fail");
+  if (!modem.waitForNetwork())
+  {
+    Serial.println("Fail");
     delay(10000);
     return;
   }
-  Serial.println("network success");
+  Serial.println("Network success");
 
-  if (modem.isNetworkConnected()) {
+  if (modem.isNetworkConnected())
+  {
     Serial.println("Network connected");
-  }
+  }*/
 }
 
-void loop() {
-    Serial.print("Connecting to APN: ");
-    Serial.print(apn);
-    if (!modem.gprsConnect(apn, gprsUser, gprsPass)) {
-        Serial.println(" fail");
-    }
-    else {    
-        // Making an HTTP POST request
-        Serial.println("GPRS connected");
-        
-        Serial.print(F("Performing HTTPS GET request... "));
-        http.connectionKeepAlive();  // Currently, this is needed for HTTPS
-        int err = http.get(resource);
-        if (err != 0) {
-          Serial.println(F("failed to connect"));
-          delay(10000);
-          return;
-        }
-
-        int status = http.responseStatusCode();
-        Serial.print(F("Response status code: "));
-        Serial.println(status);
-        if (!status) {
-          delay(10000);
-          return;
-        }
-
-        Serial.println(F("Response Headers:"));
-        while (http.headerAvailable()) {
-          String headerName = http.readHeaderName();
-          String headerValue = http.readHeaderValue();
-          Serial.println("    " + headerName + " : " + headerValue);
-        }
-
-        int length = http.contentLength();
-        if (length >= 0) {
-          Serial.print(F("Content length is: "));
-          Serial.println(length);
-        }
-        if (http.isResponseChunked()) {
-          Serial.println(F("The response is chunked"));
-        }
-
-        String body = http.responseBody();
-        Serial.println(F("Response:"));
-        Serial.println(body);
-
-        Serial.print(F("Body length is: "));
-        Serial.println(body.length());
-
-        // Shutdown
-
-        http.stop();
-        Serial.println(F("Server disconnected"));
-
-        modem.gprsDisconnect();
-        Serial.println(F("GPRS disconnected"));
-    }
+void loop()
+{
+  //requestApiLocation();
+  getGPSData();
+  Serial.println(SerialGPS.read());
+  delay(5000);
 }
